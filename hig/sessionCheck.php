60d433df6c465503d7f9c87bd8d9967f52bf893f
InGyu Hwang <dlsrb915@gmail.com>
2019-07-08 21:29:47 +0900

    sessionCheck.php

--- hig/sessionCheck.php
+++ hig/sessionCheck.php
@@ -4,9 +4,14 @@
 $userID = $_POST["userID"];
 $instanceID= $_POST["instanceID"];
 
-$sql = "SELECT * FROM User WHERE device_id='".$instanceID."'AND username='".$userID."'";
-$response = array();
-$response["success"] = $conn->query($sql);
-echo json_encode($response);
+$sql = "SELECT * FROM User WHERE device_id='".$instanceID."' AND username='".$userID."'";
+$result= $conn->query($sql);
+$arr = array('success'=>false);
+if($row = $result->fetch_assoc())
+{
+    $arr['success'] = true;
+}
+
+echo json_encode($arr);
 
 $conn->close();
