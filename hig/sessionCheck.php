a82d999c2988b32a134474fbb271651d27a306e2
InGyu Hwang <dlsrb915@gmail.com>
2019-07-08 14:46:07 +0900

    update sessionCheck

--- hig/sessionCheck.php
+++ hig/sessionCheck.php
@@ -4,7 +4,7 @@
 $userID = $_POST["userID"];
 $instanceID= $_POST["instanceID"];
 
-$sql = "SELECT * FROM User device_id='".$instanceID."' WHERE username='".$userID."'";
+$sql = "SELECT * FROM User WHERE device_id='".$instanceID."'AND username='".$userID."'";
 $response = array();
 $response["success"] = $conn->query($sql);
 echo json_encode($response);
