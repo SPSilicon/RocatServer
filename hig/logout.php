74ffe3d95c936263cd951c62d44e542b1adfb0c4
InGyu Hwang <dlsrb915@gmail.com>
2019-07-10 21:13:54 +0900

    logout and regist

---
+++ hig/logout.php
@@ -0,0 +1,10 @@
+<?php
+ require_once("./private/dbaccess.php");
+
+ $user_id = $_POST["userid"];
+ $device_id = $_POST["deviceid"];
+
+$sql = "UPDATE User SET device_id=NULL WHERE device_id='".$instanceID."' AND User_id = '".$user_id."'";
+$response = array();
+$response["success"] =$conn->query($sql);
+echo json_encode($response);
--- hig/registInstance.php
+++ hig/registInstance.php
@@ -7,7 +7,9 @@ require_once("./private/dbaccess.php");
 
 $userID = $_POST["userID"];
 $instanceID= $_POST["instanceID"];
-// TODO : 중복된 instanceID가 db내에 있으면, 삭제후 현 username 에 삽입
+// 중복된 instanceID가 db내에 있으면, 삭제후 현 username 에 삽입
+$sql = "UPDATE User SET device_id= NULL WHERE device_id='".$instanceID."'";
+$conn->query($sql);
 $sql = "UPDATE User SET device_id='".$instanceID."' WHERE username='".$userID."'";
 $response = array();
 $response["success"] =$conn->query($sql);
