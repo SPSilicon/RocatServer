c3deabb290b7c4824f95e63d3b5e7047c5cdbd64
InGyu Hwang <dlsrb915@gmail.com>
2019-07-12 12:13:40 +0900

    logout update

--- hig/logout.php
+++ hig/logout.php
@@ -1,10 +1,15 @@
 <?php
  require_once("./private/dbaccess.php");
 // 로그아웃할 시, 아이디에 등록된 device_id(instance_id) 제거
- $user_id = $_POST["userid"];
- $device_id = $_POST["deviceid"];
+ $user_id = $_POST["userID"];
+ $device_id = $_POST["deviceID"];
 
-$sql = "UPDATE User SET device_id=NULL WHERE device_id='".$instanceID."' AND User_id = '".$user_id."'";
+$sql = "UPDATE User SET device_id=NULL WHERE device_id='".$device_id."' AND username = '".$user_id."'";
 $response = array();
-$response["success"] =$conn->query($sql);
+$result = $conn->query($sql);
+if($conn->affected_rows > 0)     // 1열이상 적용될 시,
+$response["success"] = true;
+else
+$response["success"] = false;
 echo json_encode($response);
+
