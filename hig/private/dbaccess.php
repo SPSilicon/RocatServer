d28b801a912f3a2133c1a6fad3292548a7ce8c97
SPSilicon <44769598+SPSilicon@users.noreply.github.com>
2023-02-01 13:01:14 +0900

    Update dbaccess.php
    ^^

--- hig/private/dbaccess.php
+++ hig/private/dbaccess.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1:3306";
 $username = "root";
-$password = "hig0722";
+$password = "---";
 $dbname = "rocat";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
